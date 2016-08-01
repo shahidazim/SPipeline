ceeb0316602490dae5d8f905e9564bb08746eba5
sazim <sazim@sunbrandingsolutions.com>
2016-08-01 13:50:57 +0100

    Added Generic Pipeline unit test

---
+++ test/SPipeline.Pipeline.Test/GenericPipelineTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPipeline.Core.Interfaces;
+
+namespace SPipeline.Pipeline.Test
+{
+    [TestClass]
+    public class GenericPipelineTest
+    {
+
+        public class GenericActionRequest : ActionRequestBase
+        {
+        }
+
+        public class GenericActionResponse : ActionResponseBase
+        {
+        }
+
+        public class GenericActionHandler<TTranslateRequest, TTranslateResponse>
+            : ActionHandlerBase<GenericActionRequest, GenericActionResponse, TTranslateRequest, TTranslateResponse>
+            where TTranslateRequest : IMessageRequest
+            where TTranslateResponse : IMessageResponse
+        {
+            public GenericActionHandler(
+                Func<TTranslateRequest, GenericActionRequest> requestTranslator,
+                Func<GenericActionResponse, TTranslateResponse> responseTranslator)
+                : base(requestTranslator, responseTranslator)
+            {
+            }
+
+            public override GenericActionResponse Execute(GenericActionRequest actionRequest)
+            {
+                return new GenericActionResponse();
+            }
+        }
+
+        public class GenericPipelineRequest : MessageRequestBase
+        {
+            public GenericPipelineRequest(bool clearErrorsBeforeNextHandler) : base(clearErrorsBeforeNextHandler)
+            {
+            }
+        }
+
+        public class GenericPipelineResponse : MessageResponseBase
+        {
+        }
+
+        [TestMethod]
+        public void Generic_Pipeline_Test()
+        {
+            var pipeline = new GenericPipeline<GenericPipelineRequest, GenericPipelineResponse>();
+            var response = pipeline.Execute(new GenericPipelineRequest(false));
+            Assert.IsInstanceOfType(response, typeof(GenericPipelineResponse));
+        }
+    }
+}
